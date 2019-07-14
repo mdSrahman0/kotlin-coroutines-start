f3a53fa19d1f5a009c9f2f1689ab20bf6ec9c17c
Md Rahman <md.rahman@mobileappscompany.us>
2019-07-14 19:18:35 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.kotlincoroutines.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.android.kotlincoroutines.util.BACKGROUND
+import kotlinx.coroutines.*
+
+/**
+ * MainViewModel designed to store and manage UI-related data in a lifecycle conscious way. This
+ * allows data to survive configuration changes such as screen rotations. In addition, background
+ * work such as fetching network results can continue through configuration changes and deliver
+ * results after the new Fragment or Activity is available.
+ */
+class MainViewModel : ViewModel() {
+
+    private val viewModelJob = Job()
+
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    // Make a network request without blocking the UI thread
+    private fun makeNetworkRequest() {
+        // launch a coroutine in viewModelScope
+        viewModelScope.launch(Dispatchers.IO) {
+            // slowFetch()
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel();
+    }
+    /**
+     * Request a snackbar to display a string.
+     *
+     * This variable is private because we don't want to expose MutableLiveData
+     *
+     * MutableLiveData allows anyone to set a value, and MainViewModel is the only
+     * class that should be setting values.
+     */
+    private val _snackBar = MutableLiveData<String>()
+
+    /**
+     * Request a snackbar to display a string.
+     */
+    val snackbar: LiveData<String>
+        get() = _snackBar
+
+    /**
+     * Wait one second then display a snackbar.
+     */
+    fun onMainViewClicked() {
+        // launch a coroutine in viewModelScope
+        viewModelScope.launch {
+            // suspend this coroutine for one second
+            delay(1_000)
+            // resume in the main dispatcher
+            // _snackbar.value can be called directly from main thread
+            _snackBar.value = "Hello, from coroutines!"
+        }
+    }
+
+    /**
+     * Called immediately after the UI shows the snackbar.
+     */
+    fun onSnackbarShown() {
+        _snackBar.value = null
+    }
+}
